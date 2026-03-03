184eef39439ddbb02074b39eb7808823876445aa
gimu02746 <gimu02746@gmail.com>
2026-03-03 13:47:06 +0900

    Add game constants and configuration values

---
+++ src/Shared/Constants.lua
@@ -0,0 +1,14 @@
+-- Game Constants and Configuration Values
+
+local Constants = {
+    GAME_NAME = "My Awesome Game",
+    MAX_PLAYERS = 4,
+    INITIAL_HEALTH = 100,
+    GRAVITY = 9.81,
+    PLAYER_SPEED = 5,
+    JUMP_HEIGHT = 1.5,
+    ENEMY_DAMAGE = 10,
+    SPAWN_RATE = 2, -- enemies per minute
+}
+
+return Constants
